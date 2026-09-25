b04ac7492fdeb4a0a337db673126292015924fd7
vinnlops <vinilloops@gmail.com>
2026-09-25 13:57:29 -0300

    feat: nova section de componentes adicionado parcialmente com botões

--- src/features/landing/pages/LandingPage.tsx
+++ src/features/landing/pages/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/shared/components";
 import { HeaderSection } from "../components/sections/HeaderSection";
 import { ThemesSection } from "../components/sections/ThemesSection";
 
@@ -7,6 +8,60 @@ function LandingPage() {
       <HeaderSection />
 
       <ThemesSection />
+
+      <div className="flex flex-col items-center content-center gap-12 justify-center py-20">
+        <div className="flex flex-col">
+          <h2 className="text-muted-900 shadow-text-1 tracking-widest text-5xl transition-all transition-slow ease-responsive">
+            Componentes
+          </h2>
+        </div>
+
+        <div className="flex flex-col gap-6 w-full">
+          <div className="grid grid-cols-2 gap-3">
+            <div className="flex flex-col gap-3 justify-center items-center">
+              <h3 className="text-muted-900 shadow-text-1 tracking-widest text-4xl transition-all transition-slow ease-responsive">
+                Botões
+              </h3>
+            </div>
+            <div className="flex flex-row gap-3 justify-center items-center">
+              <Button
+                rounded={"md"}
+                className="shadow-box-1"
+                bgVariant={"primary"}
+              >
+                Primary
+              </Button>
+              <Button
+                rounded={"md"}
+                className="shadow-box-1"
+                bgVariant={"secondary"}
+              >
+                Secondary
+              </Button>
+              <Button
+                rounded={"md"}
+                className="shadow-box-1"
+                bgVariant={"danger"}
+              >
+                danger
+              </Button>
+              <Button
+                rounded={"md"}
+                className="shadow-box-1"
+                bgVariant={"outline"}
+              >
+                Outline
+              </Button>
+              <Button
+                rounded={"md"}
+                bgVariant={"ghost"}
+              >
+                Ghost
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
